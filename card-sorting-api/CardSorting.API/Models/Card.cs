d4466421ab7a6aa88e30f257a92e7d3fc10dfd0b
Rajshree Sahu <rajshree.sahu@intel.com>
2024-10-19 18:52:02 +0530

    Making SpecialType nullable

--- card-sorting-api/CardSorting.API/Models/Card.cs
+++ card-sorting-api/CardSorting.API/Models/Card.cs
@@ -4,6 +4,6 @@
     {
         public string Rank { get; set; } //Rank of the card, "4", "A", etc
         public char Suit { get; set; } //Suit of the card, "Diamonds(D), Spades(S)
-        public string SpecialType { get; set; } // Special Cards like "4T", "PT", etc or null for regular cards
+        public string? SpecialType { get; set; } // Special Cards like "4T", "PT", etc or null for regular cards
     }
 }
